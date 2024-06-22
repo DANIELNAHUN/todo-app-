ed716e2167756973482c1d9ed47bec7adf3ffb5f
DANIELNAHUN <nahuncf5@gmail.com>
2024-06-21 23:47:16 -0500

    add validation for user in permissions

--- routes/usuarios.py
+++ routes/usuarios.py
@@ -1,15 +1,17 @@
-from json import load
 import os
 from datetime import datetime, timedelta
+from json import load
 from typing import Annotated, List
+
 from dotenv import load_dotenv
-
 from fastapi import APIRouter, Depends, HTTPException, Response
-from starlette.status import HTTP_404_NOT_FOUND, HTTP_201_CREATED, HTTP_204_NO_CONTENT
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
-from werkzeug.security import generate_password_hash, check_password_hash
-from jose import jwt, JWTError
+from starlette.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
+                              HTTP_404_NOT_FOUND)
+from werkzeug.security import check_password_hash, generate_password_hash
 
+import models.params as models_params
 import models.usuarios as models_user
 import schemas.usuarios as schemas_user
 from config.db_todo import SessionLocal
@@ -35,9 +37,16 @@ EMPLEADOS FUNCTIONS
 """
 
 @usuarios.get("/empleados", tags= ["Operaciones Empleados"], response_model=List[schemas_user.Empleados])
-async def get_empleados(db: db_dependency):
-    empleados_d = db.query(models_user.Empleados).all()
-    return empleados_d
+async def get_empleados(db: db_dependency, token: str):
+    user = get_user_by_usertoken(db=db, token=token)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    list_permisos = get_permisos_by_user(db=db, id_usuario=user.id_usuario)
+    if 8 in list_permisos:
+        empleados_d = db.query(models_user.Empleados).all()
+        return empleados_d
+    else:
+        raise HTTPException(status_code=403, detail="Acceso denegado")
 
 @usuarios.get("/empleado/{id_empleado}", response_model=schemas_user.Empleados, tags= ["Operaciones Empleados"])
 async def get_empleado(id_empleado: int, db: db_dependency):
@@ -95,17 +104,39 @@ USUARIOS FUNCTIONS
 ༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄༄
 
 """
-def get_user(db: db_dependency, username: str):
+def get_user_by_username(db: db_dependency, username: str):
     usuario = db.query(models_user.Usuarios).filter(models_user.Usuarios.username == username).first()
     if usuario is None:
         return []
     return usuario
 
+def get_user_by_usertoken(db: db_dependency, token: str):
+    usuario = db.query(models_user.Usuarios).filter(models_user.Usuarios.usertoken == token).first()
+    if usuario is None:
+        return []
+    return usuario
+
+def get_permisos_by_user(db: db_dependency, id_usuario: int):
+    permisos_user = []
+    roles = db.query(models_params.UsuariosRoles).filter(models_params.UsuariosRoles.id_usuario == id_usuario).all()
+    roles_permisos = db.query(models_params.RolesPermisos).filter(models_params.RolesPermisos.id_rol.in_([role.id_rol for role in roles])).all()
+    permisos = db.query(models_params.UsuariosPermisos).filter(models_params.UsuariosPermisos.id_usuario == id_usuario).all()
+    permisos_by_user = [permiso.id_permiso for permiso in permisos]
+    permisos_by_rol = [permiso.id_permiso for permiso in roles_permisos]
+    if len(permisos_by_rol) > 0:
+        permisos_user.extend(permisos_by_rol)
+    if len(permisos_by_user) > 0:
+        permisos_user.extend(permisos_by_user)
+    if len(permisos_user) == 0:
+        return []
+    return permisos_user
+
+
 def verify_password(password: str, password_hash: str):
     return check_password_hash(password_hash, password)
 
 def authenticate_user(db: db_dependency, username: str, password: str):
-    usuario = get_user(db, username)
+    usuario = get_user_by_username(db, username)
     if not usuario:
         raise HTTPException(status_code=401, detail="Usuario no encontrado", headers={"WWW-Authenticate": "Bearer"})
     if not verify_password(password, usuario.userpassword):
@@ -133,5 +164,22 @@ async def login_user(db: db_dependency, user: schemas_user.Usuarios):
     usuario_d = authenticate_user(db, user.username, user.userpassword)
     if not usuario_d:
         raise HTTPException(status_code=401, detail="Usuario no encontrado", headers={"WWW-Authenticate": "Bearer"})
-    token = create_token({"sub": usuario_d.username})
-    return token
+    if not usuario_d.usertoken:
+        token = create_token({"sub": usuario_d.username})
+        usuario_d.usertoken = token
+    else:
+        token = usuario_d.usertoken
+    db.commit()
+    db.refresh(usuario_d)
+    return usuario_d
+
+@usuarios.post("/logout", tags= ["Operaciones Usuarios"])
+async def logout_user(db: db_dependency, user: schemas_user.Usuarios):
+    usuario_d = get_user_by_username(db, user.username)
+    if not usuario_d:
+        raise HTTPException(status_code=401, detail="Usuario no encontrado", headers={"WWW-Authenticate": "Bearer"})
+    usuario_d.usertoken = None
+    db.commit()
+    db.refresh(usuario_d)
+    return usuario_d
+
